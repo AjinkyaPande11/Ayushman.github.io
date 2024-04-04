1fabd7418f38a451031a36de90cb108e2bcdc05b
ayushman601 <ayushsahuji601@gmail.com>
2024-04-04 23:48:20 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import Navbar from './component/Navbarmenu';
 import Main from './component/main';
 import All from './component/all';
+import Login from './component/login';
+import ContactForm from './component/ConatctForm';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <>
     <div>
-    <Navbar/>
-    <Main/>
+   
+    <Router>
+      <div>
+        {/* <Login/> */}
+        <Navbar/>
+        {/* <Main/> */}
+      <Routes>
+        <Route path="/HOME" element = {<Main/>} />
+        <Route path="/contacts" element = {<Main/>} />
+        <Route path="/edit" element = {<Login/>} />
+        {/* <Route path="/COMPONENTS" element = {<ContactForm/>} /> */}
+      
+        
+      </Routes>
+      </div>
+    </Router>
     {/* <All/> */}
     </div>
     </>
--- src/component/Navbarmenu.js
+++ src/component/Navbarmenu.js
@@ -1,20 +1,20 @@
-import './Navbar.css'; // Import the CSS file for styling
-
+import './Navbar.css';
+// Import the CSS file for styling
+import {Link} from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="container">
         <a className="navbar-brand" href="index.html">
-          <img src="./assets/techmaniacs_iitt_logo-removebg-preview.png" alt="Topic Image" />
+          <img src='/' alt="logo" />
           
         </a>
 
         <div className="navbar-links">
-          <a href="#section_1">Home</a>
-          <a href="#section_2">Browse Topics</a>
-          <a href="#section_3">How it works</a>
-          <a href="#section_4">FAQs</a>
-          <a href="#section_5">Contact</a>
+          <Link className='items' id='jitem_1' to="/HOME">Home</Link>
+          <Link className='items' id='jitem_2' to="/CONTACTS">Contacts</Link>
+          <Link className='items' id='jitem_3' to="/EDIT">Edit</Link>
+          <Link className='items' id='jitem_4' to="/COMPONENTS">Ask For Component</Link>
         </div>
       </div>
     </nav>
